6d39d79f90c2ebc033d44b316031e11a3fdd7b9c
Dhaya3132 <dhayalanu103@gmail.com>
2024-08-03 19:17:43 +0530

    Feat - Filter by Rating

--- src/Components/FoodItem.jsx
+++ src/Components/FoodItem.jsx
@@ -6,36 +6,39 @@ import { useState } from "react";
 import Filter from "./Filter";
 
 function Food_Item() {
-    const [searchText, setSearchText] = useState('');
+    const [filterdFood, setFilteredFood] = useState(FoodMenu)
+
     const onSearchtext = (input) => {
-        setSearchText(input)
+        const FilterFood = FoodMenu.filter((item) => (
+            item.title.toLowerCase().includes(input.toLowerCase())
+        ))
+        setFilteredFood(FilterFood)
     }
-
-    const FilterFood = FoodMenu.filter((item)=> (
-        item.title.toLowerCase().includes(searchText.toLowerCase())
-    ))
-
-    return (
-        <div>
-            <Search productSearch={onSearchtext} />
-            <Filter />
-            <div id="products" className="mt-10 flex flex-wrap justify-center items-center gap-9">
-                {FilterFood.map((item) => (
-                    <div id="foot_item" key={item.id}>
-                        <img src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${item.img}`} alt="FoodImage" className="w-72 h-48 rounded-3xl object-cover" />
-                        <h2 className="font-bold text-xl m-2">{item.title}</h2>
-                        <div className="flex items-center gap-1">
-                            <FontAwesomeIcon icon={faStar} className="bg-orange-50 rounded-full p-1" style={{ color: "#FFD43B", }} />
-                            <p className="font-semibold text-lg text-slate-500 ">{item.Rating}</p>
-                        </div>
-                        <h6 className="text-lg font-semibold m-2">{item.location}</h6>
-                        <button className="text-white bg-orange-600 px-6 py-2 rounded-full font-semibold">Add to Cart</button>
-
+    const onFiltering = () => {
+        const FilerRating = filterdFood.filter((item) => item.Rating >= '4.3')
+        setFilteredFood(FilerRating)
+    }
+return (
+    <div>
+        <Search productSearch={onSearchtext} />
+        <Filter filterrating = {onFiltering}/>
+        <div id="products" className="mt-10 flex flex-wrap justify-center items-center gap-9">
+            {filterdFood.map((item) => (
+                <div id="foot_item" key={item.id}>
+                    <img src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${item.img}`} alt="FoodImage" className="w-72 h-48 rounded-3xl object-cover" />
+                    <h2 className="font-bold text-xl m-2">{item.title}</h2>
+                    <div className="flex items-center gap-1">
+                        <FontAwesomeIcon icon={faStar} className="bg-orange-50 rounded-full p-1" style={{ color: "#FFD43B", }} />
+                        <p className="font-semibold text-lg text-slate-500 ">{item.Rating}</p>
                     </div>
-                ))}
-            </div>
-        </div>
+                    <h6 className="text-lg font-semibold m-2">{item.location}</h6>
+                    <button className="text-white bg-orange-600 px-6 py-2 rounded-full font-semibold">Add to Cart</button>
 
-    )
+                </div>
+            ))}
+        </div>
+    </div>
+
+)
 }
 export default Food_Item;
